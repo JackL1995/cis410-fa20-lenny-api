6ee207538b97416f108794e023a3207d35055b57
JackL1995 <jlenny@colostate.edu>
2020-11-03 16:39:01 -0700

    10.1 10/27 /vehicle/:pk

--- index.js
+++ index.js
@@ -22,9 +22,36 @@ app.get("/vehicle", (req,res)=>{
         res.status(200).send(result) // 200 = everything went okay
     })
     .catch((err)=>{
-        console.log(err);
+        console.log("Error in query", err);
         res.status(500).send()
     })
+})
+
+app.get("/vehicle/:pk", (req, res)=>{
+    var pk = req.params.pk
+    //console.log("my PK: ", pk)
+
+    var myQuery = `SELECT *
+    FROM Vehicle
+    LEFT JOIN VehicleMake
+    ON VehicleMake.MakeID = Vehicle.MakeID 
+    WHERE VehicleVIN = ${pk}`
+
+    db.executeQuery(myQuery)
+        .then((vehicles)=>{
+            //console.log("Vehicles: ", vehicles)
+
+            if(vehicles[0]){
+                res.send(vehicles[0])
+            } else {
+                res.status(404).send('bad request')
+
+            }
+        })
+        .catch((err)=>{
+            console.log("Error in /vehicles/pk", err)
+            res.status(500).send()
+        })
 })
 app.listen(5000, ()=>{console.log("app is running on port 5000")}) // == the port num, what to do when server running
 
